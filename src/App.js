a822ffae4f07a93af4f5c731f78b8d62d0d8a603
Cory House <Cory.House@coxautoinc.com>
2018-02-15 06:17:03 -0600

    Add initial app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
+class App extends React.Component {
+  users = [
+    { name: "Cory", email: "housecor@gmail.com" },
+    { name: "Bob", email: "bob@gmail.com" }
+  ];
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Users</h1>
+        <ul>
+          {this.users.map(user => {
+            return (
+              <li key={user.name}>
+                {user.name} - {user.email}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
